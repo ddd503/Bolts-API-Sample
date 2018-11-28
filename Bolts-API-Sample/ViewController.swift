1f4e13e2d992ad70d6b3e2e634d98e0f67da434a
ddd503 <skbsporta7@gmail.com>
2018-11-29 02:19:50 +0900

    Initial Commit

---
+++ Bolts-API-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bolts-API-Sample
+//
+//  Created by kawaharadai on 2018/11/29.
+//  Copyright © 2018 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Bolts-API-SampleTests/Bolts_API_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bolts_API_SampleTests.swift
+//  Bolts-API-SampleTests
+//
+//  Created by kawaharadai on 2018/11/29.
+//  Copyright © 2018 kawaharadai. All rights reserved.
+//
+
+import XCTest
+@testable import Bolts_API_Sample
+
+class Bolts_API_SampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
